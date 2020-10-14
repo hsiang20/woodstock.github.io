07a98f105eb46047cdb9fe9cd2456e75afeb3c81
hsiang20 <h511168@nehs.hc.edu.tw>
2020-10-14 12:48:25 +0800

    first template

---
+++ main.js
@@ -0,0 +1,26 @@
+data = [
+{username:"dao", score:80},
+{username:"meow", score:100}
+];
+data.sort(function(a,b){return b.score-a.score});
+console.log(document.getElementsByClassName("table_dao")[0]);
+var meow = setInterval(Update(),1000);
+
+function Update(){
+    console.log(document.getElementById("table"));
+    node = document.getElementById("table").children[0];
+    console.log(node);
+    while(node.children.length > 1){
+        node.removeChild(node.children[1]);
+    }
+    for(let i = 0; i < data.length; i++){
+        row = document.createElement("tr");
+        user = document.createElement("td");
+        score = document.createElement("td");
+        user.innerHTML = data[i].username;
+        score.innerHTML = data[i].score;
+        row.appendChild(user);
+        row.appendChild(score);
+        node.appendChild(row);
+    }
+}
